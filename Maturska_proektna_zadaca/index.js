278d4ffe4a15ad7fd04b4b16c2cb7c03a2ef9804
Ognen011 <tactiletoast9@gmail.com>
2025-03-03 16:31:02 +0100

    Add files via upload

---
+++ Maturska_proektna_zadaca/index.js
@@ -0,0 +1,28 @@
+const header = document.querySelector("header");
+const aboutus =document.querySelector(".about-wrapper")
+const aboutImage=document.querySelector(".aboutImage")
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { 
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});    
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY || window.pageYOffset; 
+    var targetPosition = 1800; 
+  
+    if (scrollPosition >= targetPosition) {
+        aboutus.style.transition="1s"
+        aboutus.style.opacity="100%"
+        aboutus.style.translate="0%"
+        aboutImage.style.transition="3s"
+        aboutImage.style.translate="0%"
+
+    }
+    if(scrollPosition<targetPosition){
+        aboutus.style.opacity="0%"
+        aboutus.style.translate="-100%"
+    }
+  });
