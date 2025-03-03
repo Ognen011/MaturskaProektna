eadf658bd6ce60bdc0733852aacce8dbef361972
Ognen011 <tactiletoast9@gmail.com>
2025-03-03 17:43:10 +0100

    Add files via upload

--- Maturska_proektna_zadaca/index.js
+++ Maturska_proektna_zadaca/index.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 const aboutus =document.querySelector(".about-wrapper")
 const aboutImage=document.querySelector(".aboutImage")
-
+const contact=document.querySelector(".container")
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) { 
         header.classList.add('scrolled');
@@ -11,9 +11,10 @@ window.addEventListener('scroll', () => {
 });    
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY || window.pageYOffset; 
-    var targetPosition = 1800; 
+    var targetPosition1 = 1800; 
+    var targetPosition2 = 2400;
   
-    if (scrollPosition >= targetPosition) {
+    if (scrollPosition >= targetPosition1) {
         aboutus.style.transition="1s"
         aboutus.style.opacity="100%"
         aboutus.style.translate="0%"
@@ -21,8 +22,18 @@ window.addEventListener('scroll', function() {
         aboutImage.style.translate="0%"
 
     }
-    if(scrollPosition<targetPosition){
+    if(scrollPosition<targetPosition1||scrollPosition>targetPosition1+800){
         aboutus.style.opacity="0%"
         aboutus.style.translate="-100%"
+    }
+    if (scrollPosition >= targetPosition2) {
+        contact.style.transition="1s"
+        contact.style.opacity="100%"
+        contact.style.translate="0%"
+
+    }
+    if(scrollPosition<targetPosition2||scrollPosition>targetPosition2+800){
+        contact.style.opacity="0%"
+        contact.style.translate="-100%"
     }
   });
